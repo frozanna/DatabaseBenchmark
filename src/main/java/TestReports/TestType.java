cd7448f8794e529d3f181f576f40ab17f5ae8e0d
frozanna <ania.s.731@gmail.com>
2022-05-29 11:19:59 +0200

    Small fix in graph test

--- src/main/java/TestReports/TestType.java
+++ src/main/java/TestReports/TestType.java
@@ -15,7 +15,7 @@ public enum TestType {
     GROUP_BY("GroupByTest", TestCategory.QUERY),
 
     GET_NEIGHBOURS("GetNeighboursTest", TestCategory.QUERY),
-    GET_VERTICES_WITHOUT_EDGES("GetVerticesWithoutEdgesTest", TestCategory.QUERY);
+    GET_VERTICES_WITHOUT_EDGES("GetVerticesWithoutEdgesTest", TestCategory.QUERY),
     GET_COMMON_NEIGHBOURS("GetCommonNeighboursTest", TestCategory.QUERY),
     PATH_EXISTENCE("PathExistenceTest", TestCategory.QUERY);
 
